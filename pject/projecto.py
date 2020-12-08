1152801bc5fb4ca136bbc5b082362c1330dca940
Albert Chu <albertchu94@outlook.com>
2020-12-08 22:54:07 +0000

    dogcatproject

---
+++ pject/projecto.py
@@ -0,0 +1,22 @@
+df List and Dicitionary Challenge!
+# 
+# Create a list of dog names (at least 3)
+dogs = ['tori', 'bud', 'joy']
+# Create another list of cat names (at least 3)
+cats = ['purr', 'kit', 'kat']
+# Append the cats list onto the dogs list (so it is a single list)
+dogs.append(cats)
+# Print out the first dog name from your single list
+print(dogs[0])
+# Print out the second cat name from your single list
+print(cats[1])
+# Create a dictionary with the keys "first_dog" and "second_cat".
+pets = {'first_dog': 'tori', 'second_cat': 'kit'}
+# Use the appropriate values from your list as the values for the dictionary keys
+
+# Print out the dictionary
+print(pets)
+# Print out the value of "first_dog" and "second_cat" from the dictionary
+print(pets['first_dog'])
+print(pets['second_cat'])
+
