0b380f4ae7a8d07fc363d424d97bd9590989b066
Albert Chu <albertchu94@outlook.com>
2020-12-09 20:02:45 +0000

    New_Mbps

--- custif/myflix.py
+++ custif/myflix.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 
-# message = 'The movie is about to begin, we recommend' 
 message = 'The speed of your network and performance '
-# wrap connection in a float() to accept decimals as numbers
-# connection = float(input("What is your connection speed in Mbps?"))
 connection = float(input("What is your speed/performance"))
 if connection >= 200:
     message = message + 'is amazing.'
@@ -16,16 +13,5 @@ elif connection >= 25:
 else: 
     message = message + 'is non-existant.'
 print(message)
-
-# if input value was higher or equal to 25
-# if connection >= 25:
-#    message = message + 'setting video to 4k.'
-# elif connection >= 5:
-#    message = message + 'setting video to 1080p.'
-# elif connection >= 2:
-#    message = message + 'setting video to 720p.'
-# else:
-#    message = message + 'finding another access network.'
-#print(message)
 
 
